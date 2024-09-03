893c8f989b583bff6086af86632ad3cce0ce4891
MaketendoDev <najim.oria59@hotmail.com>
2024-09-03 20:46:39 +0100

    Added Boom Boom Protocal

---
+++ src/main/java/net/maketendo/legacyheroes/procedures/BoomBoomProtocalProcedure.java
@@ -0,0 +1,11 @@
+package net.maketendo.legacyheroes.procedures;
+
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.Level;
+
+public class BoomBoomProtocalProcedure {
+	public static void execute(LevelAccessor world, double x, double y, double z) {
+		if (world instanceof Level _level && !_level.isClientSide())
+			_level.explode(null, x, y, z, 200, Level.ExplosionInteraction.BLOCK);
+	}
+}
--- src/main/java/net/maketendo/legacyheroes/procedures/HardeningMoveSetsProcedure.java
+++ src/main/java/net/maketendo/legacyheroes/procedures/HardeningMoveSetsProcedure.java
@@ -6,13 +6,15 @@ import net.minecraft.world.entity.Entity;
 import net.maketendo.legacyheroes.network.LegacyHeroesModVariables;
 
 public class HardeningMoveSetsProcedure {
-	public static void execute(LevelAccessor world, Entity entity) {
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
 			return;
 		if ((entity.getCapability(LegacyHeroesModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new LegacyHeroesModVariables.PlayerVariables())).quirk_attack_selection_rotor == 1) {
 			HardenBodyProcedure.execute(world, entity);
 		} else if ((entity.getCapability(LegacyHeroesModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new LegacyHeroesModVariables.PlayerVariables())).quirk_attack_selection_rotor == 2) {
 			RedRiotUnbreakableProcedure.execute(world, entity);
+		} else if ((entity.getCapability(LegacyHeroesModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new LegacyHeroesModVariables.PlayerVariables())).quirk_attack_selection_rotor == 3) {
+			BoomBoomProtocalProcedure.execute(world, x, y, z);
 		}
 	}
 }
